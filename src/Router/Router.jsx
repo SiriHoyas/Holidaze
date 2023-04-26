e127bc1a50776d915655d4416fd320d1db360fda
Siri Høyås <siri@skjervold.me>
2023-04-26 21:50:32 +0200

    updates router

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 
-import Authentication from "../pages/Authentication";
 import HomePage from "../pages/HomePage";
 import Layout from "../components/Layout";
 import Login from "../pages/Login";

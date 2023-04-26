c264537ecbc5342eff664224147eb04f27e866eb
Siri Høyås <siri@skjervold.me>
2023-04-26 21:56:39 +0200

    updates file path

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,7 +1,7 @@
 import { AppBar, Toolbar } from "@mui/material";
 
 import LogoHorizontal from "../../../assets/brand/LogoHorizontal";
-import MenuIcon from "../../UI/icons/MenuIcon";
+import MenuIcon from "../../../assets/icons/MenuIcon";
 
 function Header() {
   return (

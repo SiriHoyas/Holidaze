da0aafad8648c7fab6442caa4512b653bf8c7ea3
Siri Høyås <siri@skjervold.me>
2023-04-28 18:36:23 +0200

    adds login with redirect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,79 @@
-import { FormControl, Grid, TextField } from "@mui/material";
+import * as yup from "yup";
 
-import Button from "../components/Button";
+import { Checkbox, Grid, TextField } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+
+import Button from "./../components/Button";
+import UploadProfileImage from "../components/UploadProfileImage";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email()
+    .matches(/^[a-zA-Z0-9._%+-]+@(stud\.)?noroff\.no$/, "Please enter a valid email")
+    .required(),
+  password: yup.string().min(8, "Password must be eight characters or more").required(),
+});
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  console.log(email);
+  const navigate = useNavigate();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data) {
+    console.log(data);
+    const options = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    };
+
+    userLogin("https://api.noroff.dev/api/v1/holidaze/auth/login", options);
+  }
+
+  async function userLogin(url, options) {
+    try {
+      const response = await fetch(url, options);
+      console.log(response);
+
+      if (response.ok) {
+        const json = await response.json();
+        console.log(json);
+        localStorage.setItem("userName", json.name);
+        localStorage.setItem("accessToken", json.accessToken);
+        navigate("/");
+      }
+    } catch (error) {}
+  }
   return (
-    <Grid container xs={12} sx={{ mt: "6rem" }}>
-      <FormControl>
+    <Grid container sx={{ mt: "6rem" }}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Grid container gap={1}>
           <Grid item xs={12}>
-            <TextField fullWidth required id="email" label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <Controller name="email" control={control} render={({ field }) => <TextField helperText={errors.email?.message} {...field} fullWidth required id="email" label="Email" variant="outlined" />} />
           </Grid>
           <Grid item xs={12}>
-            <TextField fullWidth required id="password<" label="Password" variant="outlined" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <Controller name="password" control={control} render={({ field }) => <TextField helperText={errors.password?.message} {...field} fullWidth required id="password" label="Password" variant="outlined" type="password" />} />
           </Grid>
           <Grid item xs={12}>
-            <Button fullWidth type="submit" label={"Log in"} />
+            <Button fullWidth type="submit" label={"Login"} />
           </Grid>
         </Grid>
-      </FormControl>
+      </form>
     </Grid>
   );
 }

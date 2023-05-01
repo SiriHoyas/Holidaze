ace9393fd08129060a6a59127f846779a0594861
Siri Høyås <siri@skjervold.me>
2023-05-01 16:02:02 +0200

    adds api data to recommended carousel

--- src/components/CardGallery/index.jsx
+++ src/components/CardGallery/index.jsx
@@ -1,46 +1,32 @@
 import { Grid, ImageList, ImageListItem, ImageListItemBar, Typography } from "@mui/material";
 
-function CardGallery({ heading }) {
+function CardGallery({ heading, data }) {
   function srcset(image, size, rows = 1, cols = 1) {
     return {
       src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
       srcSet: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format&dpr=2 2x`,
     };
   }
-  const itemData = [
-    {
-      img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-      title: "Breakfast",
-      rows: 2,
-      cols: 3,
-    },
-    {
-      img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-      title: "Burger",
-      rows: 2,
-      cols: 1,
-    },
-    {
-      img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-      title: "Camera",
-      rows: 2,
-      cols: 1,
-    },
-    {
-      img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-      title: "Coffee",
-      rows: 2,
-      cols: 3,
-    },
-  ];
+
+  const itemData = data.map((item, index) => {
+    let cols;
+    if (index === 0 || index === 3) {
+      cols = 3;
+    } else {
+      cols = 1;
+    }
+
+    return { id: item.id, img: item.media[0], title: item.name, description: item.description, rows: 3, cols: cols };
+  });
+
   return (
     <Grid container direction={"column"}>
       <Typography variant="h5">{heading}</Typography>
       <ImageList variant="quilted" cols={4} rowHeight={121}>
         {itemData.map((item) => (
-          <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+          <ImageListItem key={item.id} cols={item.cols || 1} rows={item.rows || 1}>
             <img {...srcset(item.img, 121, item.rows, item.cols)} alt={item.title} loading="lazy" />
-            <ImageListItemBar sx={{ height: "5rem" }} title={item.title} subtitle={item.author} />
+            <ImageListItemBar sx={{ height: "4rem", backgroundColor: "black" }} title={item.title} subtitle={item.description} />
           </ImageListItem>
         ))}
       </ImageList>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,30 @@
-import { useDispatch, useSelector } from "react-redux";
-
-import Button from "../components/Button";
 import CardGallery from "../components/CardGallery";
 import Search from "../components/Search";
-import getBookedDates from "../js/getBookedDates";
-import { setUserInfo } from "../store/UserSlice";
+import UseApi from "../hooks/UseApi";
 
 function HomePage() {
-  getBookedDates("2023-04-30T00:00:00.000Z", "2023-05-02T00:00:00.000Z");
+  const { data } = UseApi("https://api.noroff.dev/api/v1/holidaze/venues", { method: "GET" });
+
+  let recommendedData = [];
+
+  const filterByMedia = data.filter((item) => {
+    if (item.media.length > 0) {
+      return item;
+    }
+  });
+
+  for (let i = 0; i < filterByMedia.length; i++) {
+    if (i <= 3) {
+      recommendedData.push(filterByMedia[i]);
+    }
+  }
+
+  console.log(recommendedData);
 
   return (
     <>
       <Search />
-      <CardGallery heading="Recommended" />
-      <CardGallery heading="Bring your furry friends" />
+      <CardGallery heading="Recommended" data={recommendedData} />
     </>
   );
 }

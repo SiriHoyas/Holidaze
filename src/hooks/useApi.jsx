abae5c930471cc1328675f381f5633d469d072aa
Siri Høyås <siri@skjervold.me>
2023-05-08 19:43:37 +0200

    fixes wrong name

---
+++ src/hooks/useApi.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function useApi(url, options) {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const response = await fetch(url, options);
+
+        if (response.ok) {
+          const result = await response.json();
+          setData(result);
+        }
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData();
+  }, [url]);
+  return { data, isLoading, isError };
+}
+
+export default useApi;

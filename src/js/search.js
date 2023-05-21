580ddc0033f60bfd71d823951d72947268cca928
Siri Høyås <siri@skjervold.me>
2023-05-21 22:48:16 +0200

    adds documentation for searchVenues

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,22 @@
 import { areDatesAvailable } from "./bookings";
 import { API_ROOT, MAX_SEARCH_OFFSET, VENUES_PER_BATCH, VENUES_RESULT_SIZE } from "./constants";
 
+/**
+ * Searches for venues based on the search params provided from the search bar via Redux Toolkit.
+ *
+ * The function uses VENUES_RESULT_SIZE to set a limit to how many results is stored in the return variable venues. The API does not offer a search-endpoint, and all
+ * filtering needs to be done on the front end side.This is to avoid unneccesary load on the API by repeatedly calling the API for an unlimited number of venues that matches the search params.
+ * The limit on search result size can be changed in the constants.js file.
+ *
+ * The while loop will look for matching venues as long as the venue.length (the number of matching venues is below the set result size variable, and the offset is lower than the set maximum limit offset)
+ *
+ * The function will search through the set number in VENUES_PER_BATCH at a time, and this can be adjusted for preformance.
+ *
+ * This function is dependant on the fetchVenues and filterVenues functions.
+ *
+ * @param {object} searchParams - The search params provided by the search bar. The params are stored in the search params slice in Redux Toolkit.
+ * @returns {Promise<Array>} - Returns an array of venues filtered according to the search params
+ */
 export async function searchVenues(searchParams) {
   let venues = [];
   let offset = 0;

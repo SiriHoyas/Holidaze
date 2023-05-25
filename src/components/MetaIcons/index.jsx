2910787e9963e866882df75b3a84f692b4aec1d0
Siri Høyås <siri@skjervold.me>
2023-05-25 23:15:30 +0200

    fixes meta icons

--- src/components/MetaIcons/index.jsx
+++ src/components/MetaIcons/index.jsx
@@ -22,22 +22,32 @@ function MetaIcons({ metaData, fullList }) {
     if (fullList) {
       return (
         <Grid container direction={"column"} rowGap={2}>
-          <Grid container alignItems={"center"}>
-            {parking && <ParkingIcon />}
-            <Typography sx={{ ml: ".4rem" }}>Parking available</Typography>
-          </Grid>
-          <Grid container alignItems={"center"}>
-            {breakfast && <BreakfastIcon />}
-            <Typography sx={{ ml: ".4rem" }}>Offers breakfast</Typography>
-          </Grid>
-          <Grid container alignItems={"center"}>
-            {pets && <PetIcon />}
-            <Typography sx={{ ml: ".4rem" }}>Pets allowed</Typography>
-          </Grid>
-          <Grid container alignItems={"center"}>
-            {wifi && <WifiIcon />}
-            <Typography sx={{ ml: ".4rem" }}>Free WiFi</Typography>
-          </Grid>
+          {parking && (
+            <Grid container alignItems={"center"}>
+              <ParkingIcon />
+              <Typography sx={{ ml: ".4rem" }}>Parking available</Typography>
+            </Grid>
+          )}
+
+          {breakfast && (
+            <Grid container alignItems={"center"}>
+              <BreakfastIcon />
+              <Typography sx={{ ml: ".4rem" }}>Offers breakfast</Typography>
+            </Grid>
+          )}
+
+          {pets && (
+            <Grid container alignItems={"center"}>
+              <PetIcon /> <Typography sx={{ ml: ".4rem" }}>Pets allowed</Typography>{" "}
+            </Grid>
+          )}
+
+          {wifi && (
+            <Grid container alignItems={"center"}>
+              {" "}
+              <WifiIcon /> <Typography sx={{ ml: ".4rem" }}>Free WiFi</Typography>
+            </Grid>
+          )}
         </Grid>
       );
     }

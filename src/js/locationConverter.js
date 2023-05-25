acb637c8731483478d22f35c1db8bb7487555504
Siri Høyås <siri@skjervold.me>
2023-05-26 01:33:29 +0200

    updates location converter to replace "Unknown"

--- src/js/locationConverter.js
+++ src/js/locationConverter.js
@@ -1,13 +1,15 @@
+// Just a placeholder if venue city is not defined. In my own solution, city is of course required
+// to provide if registering a new venue, but I can't control what other people do.
+// This is just for aesthetic purposes.
 export function locationConverter(venue) {
   if (venue) {
     let city;
-    if (venue.location.city !== "") {
+    if (venue.location.city === "Unknown") {
+      city = "Willowbrook Falls";
+    } else if (venue.location.city !== "") {
       city = venue.location.city;
     } else {
-      // Just a placeholder if venue city is not defined. In my own solution, city is of course required
-      // to provide if registering a new venue, but I can't control what other people do.
-      // This is just for aesthetic purposes.
-      city = "Lockwood Village";
+      city = "Harborview Haven";
     }
     return city;
   }

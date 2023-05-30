954e96f9b5cda5cab50bbf3eb6467c2daff71804
Siri Høyås <siri@skjervold.me>
2023-05-30 14:20:38 +0200

    style error message

--- src/components/ErrorMessage/index.jsx
+++ src/components/ErrorMessage/index.jsx
@@ -1,5 +1,11 @@
-function ErrorMessage({ message = "ERROR!!!!" }) {
-  return <>{message}</>;
+import { Typography } from "@mui/material";
+
+function ErrorMessage({ message = "Something went wrong. Please try again later" }) {
+  return (
+    <Typography variant="body2" color={"error.main"}>
+      {message}
+    </Typography>
+  );
 }
 
 export default ErrorMessage;
